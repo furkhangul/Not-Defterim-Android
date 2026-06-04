b52434d35dc3280996d63d0f469c0b5aecb50a72
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-06-04 16:47:22 +0300

    Update SessionManager.java

--- app/src/main/java/com/example/notdefterim/SessionManager.java
+++ app/src/main/java/com/example/notdefterim/SessionManager.java
@@ -1,3 +1,9 @@
+package com.example.notdefterim;
+import android.content.Intent; 
+import android.os.Bundle; 
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
 // Kullanıcı oturum bilgilerini yöneten sınıf
 public class SessionManager {
 
