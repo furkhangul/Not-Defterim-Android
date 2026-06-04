8d44757cf72098c167a243e5b3594501bb5af637
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-06-04 16:49:23 +0300

    Update RegisterActivity.java

--- app/src/main/java/com/example/notdefterim/RegisterActivity.java
+++ app/src/main/java/com/example/notdefterim/RegisterActivity.java
@@ -1,3 +1,13 @@
+package com.example.notdefterim;
+import android.content.Intent;
+import android.os.Bundle; 
+import android.text.TextUtils; 
+import android.view.View; 
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast; 
+import androidx.appcompat.app.AppCompatActivity;
 // Kullanıcı kayıt işlemlerini yöneten ekran
 public class RegisterActivity extends AppCompatActivity {
 
